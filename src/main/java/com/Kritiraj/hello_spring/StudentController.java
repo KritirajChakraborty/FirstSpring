ed0df439c6cbdc1eec2bfe4771daca515f5b9ebf
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2026-01-05 16:35:06 +0530

    RequestMapping and ResponseEntity

--- src/main/java/com/Kritiraj/hello_spring/StudentController.java
+++ src/main/java/com/Kritiraj/hello_spring/StudentController.java
@@ -1,47 +1,39 @@
 package com.Kritiraj.hello_spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
-    //this is how IOC works, below is bean(object created and maintained by Spring)
     @Autowired
      StudentService studentService;
-    @Autowired
-    StudentService studentService1;
 
-    //the above 2 are having the prototype annotation
-    //the below 2 are using the default singleton annotation
-    //print them both in functions to see
-    @Autowired
-    StudentRepository sr;
-    @Autowired
-    StudentRepository sr1;
 
     // API endpoints
 
-    @GetMapping("/get-student") // www.student-app.com/get-student?id=1000
-    public Student getStudentByRequestParam(@RequestParam("id") int admno) {
-        System.out.println(String.valueOf(studentService));
-        System.out.println(String.valueOf(studentService1));
-        System.out.println();
-        System.out.println(sr==sr1);
-        return studentService.getStudent(admno);
+    @GetMapping("/get-student") // www.student-app.com/student/get-student?id=1000
+    public ResponseEntity getStudentByRequestParam(@RequestParam("id") int admno) {
+        Student student = studentService.getStudent(admno);
+        return new ResponseEntity(student, HttpStatus.OK);
     }
 
-    @GetMapping("/get-student/{id}") // www.student-app.com/get-student/1000
-    public Student getStudentByPathVariable(@PathVariable("id") int admno) {
-        return studentService.getStudent(admno);
+    @GetMapping("/get-student/{id}") // www.student-app.com/student/get-student/1000
+    public ResponseEntity getStudentByPathVariable(@PathVariable("id") int admno) {
+        Student student = studentService.getStudent(admno);
+        return new ResponseEntity(student,HttpStatus.OK);
     }
 
-    @PostMapping("/add") //www.student-app.com/add
-    public String addStudent(@RequestBody Student student) {
-        return studentService.postStudent(student);
+    @PostMapping("/add") //www.student-app.com/student/add
+    public ResponseEntity addStudent(@RequestBody Student student) {
+        String message = studentService.postStudent(student);
+        return new ResponseEntity(message,HttpStatus.CREATED);
     }
 }
 
--- src/main/java/com/Kritiraj/hello_spring/StudentService.java
+++ src/main/java/com/Kritiraj/hello_spring/StudentService.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
-@Scope("prototype")
 public class StudentService {
 
     @Autowired
